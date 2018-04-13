88df486a4c37a62b11d714ba25f654cf64650a6e
Mayra Jaimes <mtj26@georgetown.edu>
2018-04-13 02:09:04 -0500

    added text to read me file

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,11 +12,22 @@ class Card extends Component {
     if (this.state.status) {
       console.log("gaveover");
       console.log(this.state.status);
+      // this.props.gameOver = () => {
+      // score reset to 0;
+      // card states = false;
+      // Then -> restart the game;
+      // }
     }
     else {
       this.setState({
         status: true
       }, () => console.log(this.state.status));
+      // this.props.shuffleCard = () => {
+      // randomize array
+      // }
+      // this.props.addScore = () => {
+      // score + 1
+      // }
     }
   }
 
